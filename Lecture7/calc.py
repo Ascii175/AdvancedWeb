41e3358f15a29aa2c6303aaf1f92185e8b7a3fc9
Ascii175 <slimeyilly@gmail.com>
2021-03-16 11:39:10 +0700

    Changes msg Lecture7

---
+++ Lecture7/calc.py
@@ -0,0 +1,18 @@
+def add(x, y):
+    """add Function"""
+    return x + y
+
+def add(x, y):
+    """Subtract Function"""
+    return x - y
+
+def add(x, y):
+    """Multiply Function"""
+    return x * y
+
+def divide(x, y):
+    """Divide Function"""
+    if y == 0:
+        raise ValueError('Can not divide by zero!')
+    return x // y
+
